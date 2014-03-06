02167da83e5a4a552687e4957fbb2873b244ddb5
Vacheslav Starikov <slavicstar@gmail.com>
2014-03-06 16:24:53 +0200

    Touchpad conf for Ubuntu - xinput.sh.

---
+++ xinput.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+xinput --set-prop 14 "Device Accel Constant Deceleration" 4.3
+xinput --set-prop 14 "Device Accel Velocity Scaling" 20 
+xinput --set-prop 14 "Device Accel Profile" 1
+  
